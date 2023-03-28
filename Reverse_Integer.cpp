33b2bc905d264eb080718633812dc5666dbd719e
Tarun Saroch <123083325+TarunSaroch@users.noreply.github.com>
2023-03-28 18:07:23 +0530

    Create Reverse_Integer.cpp

---
+++ Reverse_Integer.cpp
@@ -0,0 +1,13 @@
+// C++ code
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x){
+            if( (rev > INT_MAX / 10) or (rev < INT_MIN / 10) ) return 0;
+            rev = (rev * 10) + (x % 10);
+            x = x / 10;
+        }
+        return rev;
+    }
+};
