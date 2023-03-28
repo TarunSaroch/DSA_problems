62bcc380d329772790877a36cd5fa90850584516
Tarun Saroch <123083325+TarunSaroch@users.noreply.github.com>
2023-03-28 18:17:41 +0530

    Update Reverse_Integer.cpp

--- Reverse Integer/Reverse_Integer.cpp
+++ Reverse Integer/Reverse_Integer.cpp
@@ -1,1 +1,14 @@
-a
+// c++ code
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while(x){
+            if( (rev > INT_MAX / 10) or (rev < INT_MIN / 10) ) return 0;
+            rev = (rev * 10) + (x % 10);
+            x = x / 10;
+        }
+        return rev;
+    }
+};
