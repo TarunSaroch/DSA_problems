57495f6734ea1a9d19f3610c42d5e511e77ba23a
Tarun Saroch <123083325+TarunSaroch@users.noreply.github.com>
2023-03-31 23:43:46 +0530

    Create AskYourself_04.java

---
+++ Binary Search/AskYourself_04.java
@@ -0,0 +1,35 @@
+/*
+Find Pivot Element of a Sorted and Rotated Array.
+
+Input : 9, 10, 1, 2, 3, 4, 5, 6
+output : Pivot element is at index : 2
+*/
+
+class Main {
+    public static int findPivot( int [] nums ){
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start <= end ){
+            
+            int mid = start + ( end - start ) / 2;
+            int prev = (mid - 1 + nums.length) % nums.length;
+            int next = (mid + 1) % nums.length;
+            
+            if ( nums[ mid ] < nums[ prev ] && nums[ mid ] < nums[ next]){
+                return mid;
+            }
+            else if (nums[ mid ] < nums[ end ]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int [] nums = { 9, 10, 1, 2, 3, 4, 5, 6};
+        System.out.println( "Pivot element is at index : " + findPivot( nums ) );
+    }
+}
