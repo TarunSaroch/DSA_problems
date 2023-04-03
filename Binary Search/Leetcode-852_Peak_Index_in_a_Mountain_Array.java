cf4358fda0ef83c88f554c842bcdffece458c215
Tarun Saroch <123083325+TarunSaroch@users.noreply.github.com>
2023-04-03 23:34:48 +0530

    Optimize

--- Binary Search/Leetcode-852_Peak_Index_in_a_Mountain_Array.java
+++ Binary Search/Leetcode-852_Peak_Index_in_a_Mountain_Array.java
@@ -15,6 +15,7 @@ Example 1:    Input: arr = [0,1,0]         Output: 1
 Example 2:    Input: arr = [0,2,1,0]       Output: 1
 Example 3:    Input: arr = [0,10,5,2]      Output: 1
 */
+
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int start = 0;
