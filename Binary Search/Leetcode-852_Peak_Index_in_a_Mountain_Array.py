07a3086d1745e6a0677cd77c9cda14977f27db9b
Tarun Saroch <123083325+TarunSaroch@users.noreply.github.com>
2023-04-03 23:35:09 +0530

    Optimize

--- Binary Search/Leetcode-852_Peak_Index_in_a_Mountain_Array.py
+++ Binary Search/Leetcode-852_Peak_Index_in_a_Mountain_Array.py
@@ -14,6 +14,7 @@ Example 1:    Input: arr = [0,1,0]         Output: 1
 Example 2:    Input: arr = [0,2,1,0]       Output: 1
 Example 3:    Input: arr = [0,10,5,2]      Output: 1
 """
+
 class Solution(object):
     def peakIndexInMountainArray(self, arr):
         """
